ba5f052d343f00b86182cf6806e555d18fb7f244
Mario Vicuña <mariovicunaula@gmail.com>
2026-08-20 22:30:20 -0400

    fix(restaurante): expande campos del checkout

--- build/blocks/restaurante-cart/index.asset.php
+++ build/blocks/restaurante-cart/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '35e388a5c8e5389eec1c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '2537398c0a44bca09091');
